27af3efec742221546a6f42da21b3a50d9d4deb5
Mohamed El-Zafarani <misha@MSI>
2023-09-20 11:15:05 +0200

    pchar

--- opcodes3.c
+++ opcodes3.c
@@ -21,3 +21,27 @@ void mod(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n %= (*stack)->n;
 	pop(stack, line_number);
 }
+
+/**
+ * pchar - prints the char at the top of the stack
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	int value = (*stack)->n;
+
+	if (temp == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (value >= 0 && value <= 127)
+		printf("%c\n", value);
+	else
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
